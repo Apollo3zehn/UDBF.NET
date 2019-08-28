c843c53c97c567d2014bc68004915d18a26f3902
Vincent Wilms <vwilms@outlook.com>
2019-08-28 16:33:27 +0200

    Improve binary reader copying part.

--- src/UDBF.NET/BinaryReaderExtensions.cs
+++ src/UDBF.NET/BinaryReaderExtensions.cs
@@ -25,17 +25,10 @@ namespace UDBF.NET
 
             fixed (byte* p_source_bytes = &bytes[0])
             {
-                // create pointer to value of type T
-                T* p_value = &value;
+                T* p_source_value = (T*)p_source_bytes;
+                T* p_target_value = &value;
 
-                // cast T pointer to byte pointer
-                byte* p_target_bytes = (byte*)p_value;
-
-                // copy bytes
-                for (int i = 0; i < byteCount; i++)
-                {
-                    p_target_bytes[i] = p_source_bytes[i];
-                }
+                *p_target_value = *p_source_value;
             }
 
             return value;
